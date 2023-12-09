fe05a58a47ce3cc06d17a241a4a814035b01cec5
Luizzzin <luizhenribarbodias@gmail.com>
2023-12-08 22:45:27 -0300

    up
    up

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ var botao = document.getElementById('botao')
 function abrir() {
     var minhaDiv = document.getElementById("minhaDiv");
     if (minhaDiv.style.display === "none") {
-        minhaDiv.style.display = "block";
+        minhaDiv.style.display = "flex";
+        botao.style.display = "none"
     } else {
         minhaDiv.style.display = "none";
+        botao.style.display = "block"
     }
 }
