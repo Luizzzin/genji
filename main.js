ac8b2fd6ea43f4d7c0a9a07c792201104460f7ad
Luizzzin <luizhenribarbodias@gmail.com>
2023-12-12 00:05:48 -0300

    up
    up

--- main.js
+++ main.js
@@ -60,3 +60,68 @@ prev.onclick = function () {
 }
 // ------ desenho agr
 
+// Pega o elemento canvas do HTML
+let canvas = document.getElementById("desenhar");
+
+// Pega o contexto de desenho 2D do canvas
+let contexto = canvas.getContext("2d");
+
+// Variável para identificar se estamos desenhando
+let desenhando = false;
+
+// Largura inicial do pincel
+let larguraPincel = 2;
+
+// Referência ao elemento de largura no HTML
+let largura = document.getElementById("largura");
+
+// Atualiza o conteúdo do elemento de largura
+largura.value = larguraPincel;
+
+// Define a cor padrão do pincel
+let corPincel = "black";
+
+// Adiciona eventos de mouse para detectar cliques, movimentos e solturas do mouse
+canvas.addEventListener("mousedown", function (event) {
+    desenhando = true;
+    contexto.beginPath();
+    contexto.moveTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
+});
+
+canvas.addEventListener("mousemove", function (event) {
+    if (desenhando) {
+        contexto.lineTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
+        contexto.stroke();
+    }
+});
+
+canvas.addEventListener("mouseup", function () {
+    desenhando = false;
+});
+
+// Adiciona evento de mudança para a largura do pincel
+largura.addEventListener("input", function () {
+    larguraPincel = parseInt(largura.value);
+    contexto.lineWidth = larguraPincel;
+});
+
+// Adiciona evento de mudança para a cor do pincel
+document.getElementById("inputColor").addEventListener("input", function () {
+    corPincel = document.getElementById("inputColor").value;
+    contexto.strokeStyle = corPincel;
+});
+
+// Função para aumentar a largura do pincel
+function maisLargura() {
+    larguraPincel++;
+    contexto.lineWidth = larguraPincel;
+    largura.value = larguraPincel;
+}
+
+// Função para diminuir a largura do pincel
+function menosLargura() {
+    larguraPincel--;
+    contexto.lineWidth = larguraPincel;
+    largura.value = larguraPincel < 1 ? 1 : larguraPincel;
+}
+
